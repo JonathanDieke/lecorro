512e1d2abb122450c806b5b67221e433ee9baeb6
JonathanDieke <louloulexus@gmail.com>
2020-11-09 17:05:44 +0000

    first commit

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 var apiRouter   = require('./apiRouter').router; 
-var cors = require('cors');
-const { date } = require('joi');
+var cors = require('cors'); 
 
 const app = express()
 const port = 8080
