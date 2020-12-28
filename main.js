1d197861da03d0858d279082337ea8b2e9492167
JonathanDieke <louloulexus@gmail.com>
2020-12-28 16:36:16 +0000

    sequelize sync

--- main.js
+++ main.js
@@ -4,15 +4,18 @@ var apiRouter   = require('./apiRouter').router;
 var corroRouter   = require('./corroRouter').router; 
 const path = require('path')
 var cors = require('cors'); 
-var models    = require('./models');
+const {sequelize} = require('./models');
+// var models    = require('./models');
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+// const { Pool } = require('pg');
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+
 
 
 const app = express()
@@ -24,28 +27,23 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(cors())
 
-// Route test
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
 //Configuration des routes 
 app.use('/api/', apiRouter);
 app.use('/', corroRouter);
 
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM test_table');
-    const results = { 'results': (result) ? result.rows : null};
-    res.send( {results ,  "env" : process.env});
+// app.get('/db', async (req, res) => {
+//   try {
+//     const client = await pool.connect();
+//     const result = await client.query('SELECT * FROM test_table');
+//     const results = { 'results': (result) ? result.rows : null};
+//     res.send( {results ,  "env" : process.env});
 
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-})
+//     client.release();
+//   } catch (err) {
+//     console.error(err);
+//     res.send("Error " + err);
+//   }
+// })
 
 if(process.env.NODE_ENV === "production"){
   app.use(express.static("client/build"))
@@ -55,18 +53,14 @@ if(process.env.NODE_ENV === "production"){
   })
 }
 
-app.listen(port, () => {
-  const d = new Date()
-  console.log(`App listening at http://localhost:${port} - ${d.getHours()}h:${d.getMinutes()}min `)
-  
-  // models.Level.findOne({
-  //   attributes  : ['id', "libel"],
-  //   where :  {id: 1}, 
-  //   include : [{model : models.Study,}]
-  // }).then(level => {
-  //   level.Studies.forEach(study => {
-  //     console.log(study.createdAt.getHours())
-  //   });
-  // })
 
+sequelize.sync()
+  .then( () => {
+    console.log("connection à la bd établie ");
+  });
+
+
+app.listen(port, () => {
+  const d = new Date();
+  console.log(`App listening at http://localhost:${port} - ${d.getHours()}h:${d.getMinutes()}min `);
 })
